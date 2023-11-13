441f10c95e74e86c9cc15e45d469538d5c97a63a
Ilya Shamrov <ilya66401@gmail.com>
2023-11-13 21:08:54 +0300

    Finishes CRUD

--- common.py
+++ common.py
@@ -13,7 +13,7 @@ def addingVacancyCollection(vacancyNameEntry, vacancyDescriptionEntry, salaryEnt
     else:
         existingDocument = database['DB']['VacancyDocument'].count_documents({'Title': vacancyNameAdd})
         if existingDocument > 0:
-            showerror(title='Ошибка', message='Вакансия с таким названием уже существует')
+            showerror(title='Ошибка', message='Такая вакансия уже зарегистрирована')
         else:
             dictAdd = {'Title': vacancyNameAdd, 'VacancyDescription': vacancyDescriptionAdd, 'Salary': salaryAdd, 'Status': currentStatus}
             database['DB']['VacancyDocument'].insert_one(dictAdd)
@@ -30,9 +30,9 @@ def addingEmployerCollection(currentTitle, companyNameEntry, companyDescriptionE
     if(companyNameAdd == '' or companyDescriptionAdd == '' or addressCityAdd == '' or addressStreetAdd == '' or addressHouseAdd == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        existingDocument = database['DB']['EmployerDocument'].count_documents({'VacancyDocument_id': record['_id'], 'CompanyName': companyNameAdd})
+        existingDocument = database['DB']['EmployerDocument'].count_documents({'VacancyDocument_id': record['_id'], 'CompanyName': companyNameAdd, 'CompanyDescription': companyDescriptionAdd, 'AddressCity': addressCityAdd, 'AddressStreet': addressStreetAdd, 'AddressHouse': addressHouseAdd})
         if existingDocument > 0:
-            showerror(title='Ошибка', message='Компания с таким названием уже зарегистрирована на данную вакансию')
+            showerror(title='Ошибка', message='Такая компания уже зарегистрирована на данную вакансию')
         else:
             dictAdd = {'VacancyDocument_id' : record['_id'], 'CompanyName': companyNameAdd, 'CompanyDescription': companyDescriptionAdd, 'AddressCity': addressCityAdd, 'AddressStreet': addressStreetAdd, 'AddressHouse': addressHouseAdd}
             database['DB']['EmployerDocument'].insert_one(dictAdd)
@@ -49,19 +49,13 @@ def addingCandidateCollection(currentTitle, candidateNameEntry, currentGender, d
     if(candidateNameAdd == '' or dateOfBirthAdd == '' or stageAdd == '' or phoneNumberAdd == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        existingDocument = database['DB']['CandidateDocument'].count_documents({'VacancyDocument_id': record['_id'], 'Name': candidateNameAdd})
+        existingDocument = database['DB']['CandidateDocument'].count_documents({'VacancyDocument_id': record['_id'], 'Name': candidateNameAdd, 'Gender': currentGender, 'DateOfBirth': dateOfBirthAdd, 'Stage': stageAdd, 'PhoneNumber': phoneNumberAdd})
         if existingDocument > 0:
-            showerror(title='Ошибка', message='Кандидат с таким именем уже зарегистрирован на данную вакансию')
+            showerror(title='Ошибка', message='Такой кандидат уже зарегистрирован на данную вакансию')
         else:
-            existingPhoneNumber = database['DB']['CandidateDocument'].count_documents({'PhoneNumber': phoneNumberAdd})
-            if existingPhoneNumber > 0:
-                showerror(title='Ошибка', message='Введите уникальный номер телефона')
-            else
-            {
-                dictAdd = {'VacancyDocument_id': record['_id'], 'Name': candidateNameAdd, 'Gender': currentGender, 'DateOfBirth': dateOfBirthAdd, 'Stage': stageAdd, 'PhoneNumber': phoneNumberAdd}
-                database['DB']['CandidateDocument'].insert_one(dictAdd)
-                showinfo(title='Инфо', message='Данные успешно добавлены')
-            }
+            dictAdd = {'VacancyDocument_id': record['_id'], 'Name': candidateNameAdd, 'Gender': currentGender, 'DateOfBirth': dateOfBirthAdd, 'Stage': stageAdd, 'PhoneNumber': phoneNumberAdd}
+            database['DB']['CandidateDocument'].insert_one(dictAdd)
+            showinfo(title='Инфо', message='Данные успешно добавлены')
 
 #Функция измения вакансии (главная коллекция)
 def updateVacancyCollection(oldTitle, vacancyNameEntry, vacancyDescriptionEntry, salaryEntry, currentStatus):
@@ -91,9 +85,9 @@ def updateEmployerCollection(oldCompanyName, currentTitle, companyNameEntry, com
     if(companyNameUpdate == '' or companyDescriptionUpdate == '' or addressCityUpdate == '' or addressStreetUpdate == '' or addressHouseUpdate == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        existingDocument = database['DB']['EmployerDocument'].count_documents({'VacancyDocument_id': record['_id'], 'CompanyName': companyNameUpdate})
+        existingDocument = database['DB']['EmployerDocument'].count_documents({'VacancyDocument_id': record['_id'], 'CompanyName': companyNameUpdate, 'CompanyDescription': companyDescriptionUpdate, 'AddressCity': addressCityUpdate, 'AddressStreet': addressStreetUpdate, 'AddressHouse': addressHouseUpdate})
         if existingDocument > 0:
-            showerror(title='Ошибка', message='Компания с таким названием уже зарегистрирована на данную вакансию')
+            showerror(title='Ошибка', message='Такая компания уже зарегистрирована на данную вакансию')
         else:
             oldValues = {'CompanyName': oldCompanyName}
             newValues = {'$set' : {'VacancyDocument_id': record['_id'], 'CompanyName': companyNameUpdate, 'CompanyDescription': companyDescriptionUpdate, 'AddressCity': addressCityUpdate, 'AddressStreet': addressStreetUpdate, 'AddressHouse': addressHouseUpdate}}
@@ -110,18 +104,14 @@ def updateCandidateCollection(oldName, currentTitle, candidateNameEntry, current
     if(candidateNameUpdate == '' or dateOfBirthUpdate == '' or stageUpdate == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        existingDocument = database['DB']['CandidateDocument'].count_documents({'VacancyDocument_id': record['_id'], 'Name': candidateNameUpdate})
+        existingDocument = database['DB']['CandidateDocument'].count_documents({'VacancyDocument_id': record['_id'], 'Name': candidateNameUpdate, 'Gender': currentGender, 'DateOfBirth': dateOfBirthUpdate, 'Stage': stageUpdate, 'PhoneNumber': phoneNumberUpdate})
         if existingDocument > 0:
-            showerror(title='Ошибка', message='Кандидат с таким именем уже зарегистрирован на данную вакансию')
+            showerror(title='Ошибка', message='Такой кандидат уже зарегистрирован на данную вакансию')
         else:
-            existingPhoneNumber = database['DB']['CandidateDocument'].count_documents({'PhoneNumber': phoneNumberUpdate})
-            if existingPhoneNumber > 0:
-                showerror(title='Ошибка', message='Введите уникальный номер телефона')
-            else:
-                oldValues = {'Name': oldName}
-                newValues = {'$set': {'VacancyDocument_id': record['_id'], 'Name': candidateNameUpdate, 'Gender': currentGender, 'DateOfBirth': dateOfBirthUpdate, 'Stage': stageUpdate, 'PhoneNumber': phoneNumberUpdate}}
-                database['DB']['CandidateDocument'].update_one(oldValues, newValues)
-                showinfo(title='Инфо', message='Данные успешно обновлены')
+            oldValues = {'Name': oldName}
+            newValues = {'$set': {'VacancyDocument_id': record['_id'], 'Name': candidateNameUpdate, 'Gender': currentGender, 'DateOfBirth': dateOfBirthUpdate, 'Stage': stageUpdate, 'PhoneNumber': phoneNumberUpdate}}
+            database['DB']['CandidateDocument'].update_one(oldValues, newValues)
+            showinfo(title='Инфо', message='Данные успешно обновлены')
 
 #Функция удаления вакансий (главная коллекция)
 def deleteVacancyCollection(title):
@@ -165,9 +155,13 @@ def addingVacancyDenormalized(vacancyNameEntry, vacancyDescriptionEntry, salaryE
     if(vacancyNameAdd == '' or vacancyDescriptionAdd == '' or salaryAdd == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        dictAdd = {'Title': vacancyNameAdd, 'VacancyDescription': vacancyDescriptionAdd, 'Salary': salaryAdd, 'Status': currentStatus, "Employers": [], "Candidates": []}
-        database['DB']['DenormalizedDocument'].insert_one(dictAdd)
-        showinfo(title='Инфо', message='Данные успешно добавлены')
+        existingDocument = database['DB']['DenormalizedDocument'].count_documents({'Title': vacancyNameAdd})
+        if existingDocument > 0:
+            showerror(title='Ошибка', message='Вакансия с таким названием уже существует')
+        else:
+            dictAdd = {'Title': vacancyNameAdd, 'VacancyDescription': vacancyDescriptionAdd, 'Salary': salaryAdd, 'Status': currentStatus, "Employers": [], "Candidates": []}
+            database['DB']['DenormalizedDocument'].insert_one(dictAdd)
+            showinfo(title='Инфо', message='Данные успешно добавлены')
 
 #Функция добавления компании (денормализованная коллекция)
 def addingEmployerDenormalized(currentTitle, companyNameEntry, companyDescriptionEntry, addressCityEntry, addressStreetEntry, addressHouseEntry):
@@ -179,16 +173,19 @@ def addingEmployerDenormalized(currentTitle, companyNameEntry, companyDescriptio
     if(companyNameAdd == '' or companyDescriptionAdd == '' or addressCityAdd == '' or addressStreetAdd == '' or addressHouseAdd == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        record = database['DB']['DenormalizedDocument'].find_one({'Title': currentTitle})
-        dictAdd = {'CompanyName': companyNameAdd, 'CompanyDescription': companyDescriptionAdd, 'AddressCity': addressCityAdd, 'AddressStreet': addressStreetAdd, 'AddressHouse': addressHouseAdd}
-        filterCheck = {'Title': currentTitle}
-        updateOperation = {
-            '$push': {
-                'Employers': dictAdd
+        existingDocument = database['DB']['DenormalizedDocument'].count_documents({'Title': currentTitle, 'Employers.CompanyName': companyNameAdd, 'Employers.CompanyDescription': companyDescriptionAdd, 'Employers.AddressCity': addressCityAdd, 'Employers.AddressStreet': addressStreetAdd, 'Employers.AddressHouse': addressHouseAdd})
+        if existingDocument > 0:
+            showerror(title='Ошибка', message='Компания с таким названием уже существует')
+        else:
+            dictAdd = {'CompanyName': companyNameAdd, 'CompanyDescription': companyDescriptionAdd, 'AddressCity': addressCityAdd, 'AddressStreet': addressStreetAdd, 'AddressHouse': addressHouseAdd}
+            filterCheck = {'Title': currentTitle}
+            updateOperation = {
+                '$push': {
+                    'Employers': dictAdd
+                }
             }
-        }
-        database['DB']['DenormalizedDocument'].update_one(filterCheck, updateOperation)
-        showinfo(title='Инфо', message='Данные успешно добавлены')
+            database['DB']['DenormalizedDocument'].update_one(filterCheck, updateOperation)
+            showinfo(title='Инфо', message='Данные успешно добавлены')
 
 #Функция добавления кандидата (денормализованная коллекция)
 def addingCandidateDenormalized(currentTitle, candidateNameEntry, currentGender, dateOfBirthEntry, stageEntry, phoneNumberEntry):
@@ -199,16 +196,19 @@ def addingCandidateDenormalized(currentTitle, candidateNameEntry, currentGender,
     if(candidateNameAdd == '' or dateOfBirthAdd == '' or stageAdd == '' or phoneNumberAdd == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        record = database['DB']['DenormalizedDocument'].find_one({'Title': currentTitle})
-        dictAdd = {'Name': candidateNameAdd, 'Gender': currentGender, 'DateOfBirth': dateOfBirthAdd, 'Stage': stageAdd, 'PhoneNumber': phoneNumberAdd}
-        filterCheck = {'Title': currentTitle}
-        updateOperation = {
-            '$push': {
-                'Candidates': dictAdd
+        existingDocument = database['DB']['DenormalizedDocument'].count_documents({'Title': currentTitle, 'Candidates.Name': candidateNameAdd, 'Candidates.Gender': currentGender, 'Candidates.DateOfBirth': dateOfBirthAdd, 'Candidates.Stage': stageAdd, 'Candidates.PhoneNumber': phoneNumberAdd})
+        if existingDocument > 0:
+            showerror(title='Ошибка', message='Кандидат с таким именем уже существует')
+        else:
+            dictAdd = {'Name': candidateNameAdd, 'Gender': currentGender, 'DateOfBirth': dateOfBirthAdd, 'Stage': stageAdd, 'PhoneNumber': phoneNumberAdd}
+            filterCheck = {'Title': currentTitle}
+            updateOperation = {
+                '$push': {
+                    'Candidates': dictAdd
+                }
             }
-        }
-        database['DB']['DenormalizedDocument'].update_one(filterCheck, updateOperation)
-        showinfo(title='Инфо', message='Данные успешно добавлены')
+            database['DB']['DenormalizedDocument'].update_one(filterCheck, updateOperation)
+            showinfo(title='Инфо', message='Данные успешно добавлены')
 
 #Функция обновления данных вакансии (денормализованная коллекция)
 def updateVacancyCollectionDenormalized(oldTitle, vacancyNameEntry, vacancyDescriptionEntry, salaryEntry, currentStatus):
@@ -218,10 +218,14 @@ def updateVacancyCollectionDenormalized(oldTitle, vacancyNameEntry, vacancyDescr
     if(vacancyNameUpdate == '' or vacancyDescriptionUpdate == '' or salaryUpdate == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        oldValues = {'Title': oldTitle}
-        newValues = {'$set' : {'Title': vacancyNameUpdate, 'VacancyDescription': vacancyDescriptionUpdate, 'Salary': salaryUpdate, 'Status': currentStatus}}
-        database['DB']['DenormalizedDocument'].update_one(oldValues, newValues)
-        showinfo(title='Инфо', message='Данные успешно обновлены')
+        existingDocument = database['DB']['DenormalizedDocument'].count_documents({'Title': vacancyNameUpdate})
+        if existingDocument > 0:
+            showerror(title='Ошибка', message='Вакансия с таким названием уже существует')
+        else:
+            oldValues = {'Title': oldTitle}
+            newValues = {'$set' : {'Title': vacancyNameUpdate, 'VacancyDescription': vacancyDescriptionUpdate, 'Salary': salaryUpdate, 'Status': currentStatus}}
+            database['DB']['DenormalizedDocument'].update_one(oldValues, newValues)
+            showinfo(title='Инфо', message='Данные успешно обновлены')
 
 #Функция обновления данных компании (денормализованная коллекиця)
 def updateEmployerCollectionDenormalized(currentVacancy, oldName, companyNameEntry, companyDescriptionEntry, addressCityEntry, addressStreetEntry, addressHouseEntry):
@@ -233,18 +237,22 @@ def updateEmployerCollectionDenormalized(currentVacancy, oldName, companyNameEnt
     if(companyNameUpdate == '' or companyDescriptionUpdate == '' or addressCityUpdate == '' or addressStreetUpdate == '' or addressHouseUpdate == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
-        database['DB']['DenormalizedDocument'].update_one(
-            {'Title': currentVacancy, 'Employers.CompanyName': oldName},
-            {'$set': {
-                'Employers.$.CompanyName': companyNameUpdate,
-                'Employers.$.CompanyDescription': companyDescriptionUpdate,
-                'Employers.$.AddressCity': addressCityUpdate,
-                'Employers.$.AddressStreet': addressStreetUpdate,
-                'Employers.$.AddressHouse': addressHouseUpdate
+        existingDocument = database['DB']['DenormalizedDocument'].count_documents({'Title': currentVacancy, 'Employers.CompanyName': companyNameUpdate, 'Employers.CompanyDescription': companyDescriptionUpdate, 'Employers.AddressCity': addressCityUpdate, 'Employers.AddressStreet': addressStreetUpdate, 'Employers.AddressHouse': addressHouseUpdate})
+        if existingDocument > 0:
+            showerror(title='Ошибка', message='Компания с таким названием уже зарегистрирована на данную вакансию')
+        else:
+            database['DB']['DenormalizedDocument'].update_one(
+                {'Title': currentVacancy, 'Employers.CompanyName': oldName},
+                {'$set': {
+                    'Employers.$.CompanyName': companyNameUpdate,
+                    'Employers.$.CompanyDescription': companyDescriptionUpdate,
+                    'Employers.$.AddressCity': addressCityUpdate,
+                    'Employers.$.AddressStreet': addressStreetUpdate,
+                    'Employers.$.AddressHouse': addressHouseUpdate
+                    }
                 }
-            }
-        )
-        showinfo(title='Инфо', message='Данные успешно обновлены')
+            )
+            showinfo(title='Инфо', message='Данные успешно обновлены')
 
 #Функция обновления данных кандидата (денормализованная коллекция)
 def updateCandidateCollectionDenormalized(currentVacancy, oldName, candidateNameEntry, currentGender, dateOfBirthEntry, stageEntry, phoneNumberEntry):
@@ -255,6 +263,7 @@ def updateCandidateCollectionDenormalized(currentVacancy, oldName, candidateName
     if(candidateNameUpdate == '' or dateOfBirthUpdate == '' or stageUpdate == '' or phoneNumberUpdate == ''):
         showerror(title='Ошибка', message='Заполните все данные')
     else:
+        existingDocument = database['DB']['DenormalizedDocument'].count_documents({'Title': currentVacancy, 'Candidates.Name': candidateNameUpdate, 'Candidates.Gender': currentGender, 'Candidates.DateOfBirth': dateOfBirthUpdate, 'Candidates.Stage': stageUpdate, 'Candidates.PhoneNumber': phoneNumberUpdate})
         database['DB']['DenormalizedDocument'].update_one(
             {'Title': currentVacancy, 'Candidates.Name': oldName},
             {'$set': {
